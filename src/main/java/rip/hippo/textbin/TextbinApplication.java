82fc86470bd628e650b89bb7bda34a32f10181fe
Hippo <Hippah@protonmail.com>
2021-01-22 23:56:11 -0600

    Project startup

---
+++ src/main/java/rip/hippo/textbin/TextbinApplication.java
@@ -0,0 +1,13 @@
+package rip.hippo.textbin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextbinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextbinApplication.class, args);
+    }
+
+}
